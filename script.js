5ede0b77c56f454b6f7b31c8424911796ebf0710
Rajesh Kannan <krishfredrick11@gmail.com>
2023-07-23 22:22:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+const fetchingWeather = async()=>{
+	const response = await fetch(`https://openweathermap.org/api`);
+	return response.json();
+}
+
+const handleWeather = ()=>{
+	let data;
+	try {
+		data = fetchingWeather();
+		let div = document.createElement('div');
+		document.body.appendChild(div);
+		div.innerText = data;
+	} catch (error) {
+		console.log(error);
+	}
+}
